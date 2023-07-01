f0b212676e7409054a16cdf2f2eb496585e63bcd
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-07-02 01:30:55 +0500

    fs just fs

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var x = 20;
-var y = 30;
+// var x = 20;
+// var y = 30;
 
-var z = x + y;
+// var z = x + y;
 
-console.log(z)
+// console.log(z)
+
+
+const fs = require('fs');
+console.log('learning'+'node');
+
+let string = 'learning nodess';
+
+fs.writeFileSync('hello.txt',string,{encoding:'utf8'});
