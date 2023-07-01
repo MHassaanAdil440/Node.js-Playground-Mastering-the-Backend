9376fc9ee3c336406a11f3ffd4fae9bfc740ac65
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-07-02 02:10:06 +0500

    just playing with filesystem and encryption

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ function encrypt(message){
     }
     return encryptedMessage;
 }
-
-let message = 'hi there just checking the cipher'
+function decrypt(message){
+    let encryptedMessage = '';
+    for(let i=0; i<message.length; i++){
+        const char = message[i];
+        const charNum = char.charCodeAt(0);
+        const charAdd = charNum - 1;
+        const character = String.fromCharCode(charAdd)
+        encryptedMessage += character;
+    }
+    return encryptedMessage;
+}
+let message = __dirname;
 const fs = require('fs');
-fs.writeFileSync('hello.txt',encrypt(message))
+fs.writeFileSync('encrypted.txt',encrypt(message))
+fs.writeFileSync('decrypted.txt',decrypt(encrypt(message)))
